c3d5c4a911ea162947fd7dddab41fcfb52b282f0
arxing <meiswind711108@gmail.com>
2019-12-26 14:19:09 +0800

    fixed from suggestion

--- src/org/arxing/ToSimplifiedAction.java
+++ src/org/arxing/ToSimplifiedAction.java
@@ -2,7 +2,7 @@ package org.arxing;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 
@@ -10,7 +10,7 @@ public class ToSimplifiedAction extends AnAction {
 
     @Override public void actionPerformed(AnActionEvent e) {
         Project project = e.getProject();
-        Editor editor = e.getData(DataKeys.EDITOR);
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
         new Core().run(project, editor, false);
     }
 }
--- src/org/arxing/ToTraditionalAction.java
+++ src/org/arxing/ToTraditionalAction.java
@@ -2,18 +2,15 @@ package org.arxing;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.spreada.utils.chinese.ZHConverter;
 
 public class ToTraditionalAction extends AnAction {
 
     @Override public void actionPerformed(AnActionEvent e) {
         Project project = e.getProject();
-        Editor editor = e.getData(DataKeys.EDITOR);
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
         new Core().run(project, editor, true);
     }
 }
